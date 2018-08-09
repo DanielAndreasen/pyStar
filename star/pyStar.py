ea42508bbcc267ad0d04e404fe58bfc02ebf13e1
DanielAndreasen <daniel.andreasen@hotmail.com>
2018-08-09 13:29:20 +0200

    Add enum for spectral types

--- star/pyStar.py
+++ star/pyStar.py
@@ -5,6 +5,7 @@ from seismology import Seismology
 from spectroscopy import Spectroscopy
 from calibrations import Calibrations
 from enums.evolutionary_stage import Stage
+from enums.spectral_type import SpectralType
 
 import numpy as np
 from myTypes import listLikeType
@@ -12,9 +13,10 @@ from myTypes import listLikeType
 
 class Star:
 
-    def __init__(self, name: str, stage: Stage) -> None:
+    def __init__(self, name: str, stage: Stage, spectral_type: SpectralType) -> None:
         self.name: str = name
         self.stage: Stage = stage
+        self.spectral_type: SpectralType = spectral_type
         self.colourInformation: bool = False
         self.seismicInformation: bool = False
         self.spectroscopicInformation: bool = False
@@ -23,23 +25,27 @@ class Star:
         raise ValueError('Can not add together two stars')
 
     def __repr__(self) -> str:
-        info = f'Star: {self.name}. Evolutionary stage: {self.stage.value}\n'
+        info = '#' * 30 + '\n'
+        info += f'## Star: {self.name}\n'
+        info += f'## Evolutionary stage: {self.stage.value}\n'
+        info += f'## Spectral type: {self.spectral_type.value}\n'
         if self.colourInformation:
-            info += f'Colour - Teff={self.colour.Teff}K\n'
-            info += f'Colour - logg={self.colour.logg}dex\n'
-            info += f'Colour - [Fe/H]={self.colour.feh}dex\n'
+            info += f'# \n# Colour - Teff={self.colour.Teff}K\n'
+            info += f'# Colour - logg={self.colour.logg}dex\n'
+            info += f'# Colour - [Fe/H]={self.colour.feh}dex\n'
         if self.seismicInformation:
-            info += f'Seismology - density={self.seismic.density}g/cm3\n'
-            info += f'Seismology - logg={self.seismic.logg}dex\n'
-            info += f'Seismology - mass={self.seismic.mass}Msun\n'
-            info += f'Seismology - radius={self.seismic.radius}Rsun\n'
-        if self.seismicInformation:
-            info += f'Spectroscopy - Teff={self.spectroscopic.Teff}K\n'
-            info += f'Spectroscopy - logg={self.spectroscopic.logg}dex\n'
-            info += f'Spectroscopy - [Fe/H]={self.spectroscopic.feh}dex\n'
-            info += f'Spectroscopy - vmicro={self.spectroscopic.vmicro}km/s\n'
-            info += f'Spectroscopy - vmacro={self.spectroscopic.vmacro}km/s\n'
-            info += f'Spectroscopy - vsini={self.spectroscopic.vsini}km/s\n'
+            info += f'# \n# Seismology - density={self.seismic.density}g/cm3\n'
+            info += f'# Seismology - logg={self.seismic.logg}dex\n'
+            info += f'# Seismology - mass={self.seismic.mass}Msun\n'
+            info += f'# Seismology - radius={self.seismic.radius}Rsun\n'
+        if self.spectroscopicInformation:
+            info += f'# \n# Spectroscopy - Teff={self.spectroscopic.Teff}K\n'
+            info += f'# Spectroscopy - logg={self.spectroscopic.logg}dex\n'
+            info += f'# Spectroscopy - [Fe/H]={self.spectroscopic.feh}dex\n'
+            info += f'# Spectroscopy - vmicro={self.spectroscopic.vmicro}km/s\n'
+            info += f'# Spectroscopy - vmacro={self.spectroscopic.vmacro}km/s\n'
+            info += f'# Spectroscopy - vsini={self.spectroscopic.vsini}km/s\n'
+        info += '#' * 30 + '\n'
         return info
 
     def getColourInformation(self, method: str='Ramirez05', *args, **kwargs):
@@ -62,14 +68,14 @@ class Star:
         print('Values can be reached through: Star.spectroscopic.')
 
 
+if __name__ == '__main__':
+    s1 = Star('Arcturus', Stage.GIANT, SpectralType.K3)
+    s1.getSpectroscopicInformation(np.array([1, 2, 3]), np.array([1, 2, 3]))
+    s1.getColourInformation(method='Ramirez05', feh=s1.spectroscopic.feh,
+                            logg=s1.spectroscopic.logg, B=1.3, V=0.43)
+    s1.getSeismicInformation(2.96, 110.54, 4577)
 
-s1 = Star('Arcturus', Stage.GIANT)
-s1.getSpectroscopicInformation(np.array([1, 2, 3]), np.array([1, 2, 3]))
-s1.getColourInformation(method='Ramirez05', feh=s1.spectroscopic.feh,
-                        logg=s1.spectroscopic.logg, B=1.3, V=0.43)
-s1.getSeismicInformation(2.96, 110.54, 4577)
+    print(s1)
 
-print(s1)
-
-s2 = Star('Sun', Stage.DWARF)
-print(s2)
+    s2 = Star('Sun', Stage.DWARF, SpectralType.G4)
+    print(s2)
