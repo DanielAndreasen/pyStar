e6ebbcca7f446d54716315d5af4d3f8661ddb786
DanielAndreasen <daniel.andreasen@hotmail.com>
2018-08-23 21:38:31 +0200

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,24 @@ setup(
 	maintainer='Daniel T. Andreasen',
     name='pyStar',
 	version=0.2,
+	long_description=open('README.md').read(),
 	license='MIT',
     packages=find_packages(),
     url='https://github.com/DanielAndreasey/pyStar',
     cmdclass={'build_ext': build_ext},
-	package_data={'star': ['data/spec_ml_sample.hdf']}
+	package_data={'star': ['data/spec_ml_sample.hdf']},
+	install_requires=[
+		'astropy>=3.0.3',
+		'numpy>=1.15.0',
+		'matplotlib>=2.0.2',
+		'pandas>=0.23.0',
+		'dataclasses>=0.6',
+		'scikit-learn>=0.19.2',
+		'scipy>=1.1.0',
+		'tables>=3.4.4',
+		'specML>=0.3'
+	],
+	dependency_links=[
+		'https://github.com/MariaTsantaki/spectroscopy-ML/tarball/master#egg=specML-0.3'
+	]
 )
